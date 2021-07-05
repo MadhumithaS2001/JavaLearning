ff9c0b40b51a93396e7dbfa0490b0e50357a5e5f
MadhumithaS2001 <52739023+MadhumithaS2001@users.noreply.github.com>
2021-07-05 20:41:57 +0530

    Create PyramidPaternJava.java

---
+++ PyramidPaternJava.java
@@ -0,0 +1,35 @@
+// Character Pattern
+
+public class MyJavaPrg1{
+    public static void main(String[] args) {
+        int a=5;
+        for(int i=1;i<=a;i++){
+            for(char j='A';j<('A'+i);j++){
+                System.out.print(j);
+                System.out.print(' ');
+                
+            }
+        System.out.println();
+        }
+    }
+}
+
+/*
+
+Number Pattern
+
+public class MyJavaPrg1{
+    public static void main(String[] args) {
+        int a=5;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+                System.out.print(' ');
+                
+            }
+        System.out.println();
+        }
+    }
+}
+
+*/
